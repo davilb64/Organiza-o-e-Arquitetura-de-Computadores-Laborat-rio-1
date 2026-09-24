ad033ea08acad88ae46782b3c12f1a26df11fd60
Davi <162513480+davilb64@users.noreply.github.com>
2026-09-24 15:53:10 -0300

    Adiciona funções para implementações
    Todo ferramental construido para começar a lidar com as implementações de instruções e tipos de dados

--- lab1.asm
+++ lab1.asm
@@ -4,6 +4,9 @@
 	pc_text: .word 0x00000000 # vai incrementando conforme instruções
 	pc_data: .word 0x00000000 #  vai incrementando conforme espaços reservados
 	
+	acumulador_dados: .word 0 
+	contador_bytes_dados: .word 0 
+	
 	secao_atual: .word 0 # data: 0, # text: 1
 	
 	tabela_rotulos: .space 2400 # tabela onde vamos guardar os rotulos em pares (20 bytes pro texto e 4 bytes para o endereco)
@@ -13,8 +16,8 @@
 	buffer_palavra: .space 20 # espaco pra ler palavras
 	msg_erro: .string "Erro ao abrir o arquivo\n"
 	
-	buffer_saida_data: .space 2048 # arquivo de saida do data
-	buffer_saida_text: .space 2048 # arquivo de saida do text
+	buffer_saida_data: .space 32768 # arquivo de saida do data
+	buffer_saida_text: .space 32768 # arquivo de saida do text
 	
 	ponteiro_saida_data: .word 0 # pos atual data
 	ponteiro_saida_text: .word 0 # pos atual text
@@ -33,6 +36,10 @@
 	sufixo_data: .string "_data.mif"
 	
 	str_quebra_linha: .string "\n"
+	str_dois_pnts: .string " : "
+	str_pnt_virg: .string ";\n"
+	
+	buffer_hex: .space 9 # espaco pra gravar 8 letras em hex
 	
 	passagem_atual: .word 1
 	
@@ -232,10 +239,20 @@ copia_rotulo:
 	j copia_rotulo
 	
 salva_endereco_rotulo:
-	la t1, pc_text # pega o pc da text
+	# verifica secao
+	la t1, secao_atual
 	lw t2, 0(t1)
+	beq t2, zero, grava_em_data
 	
-	# guarda o end apos o nome do rotulo
+	la t1, pc_text # grava em text
+	j grava_pc_tabela
+	
+grava_em_data:
+	la t1, pc_data
+	
+grava_pc_tabela:
+	lw t2, 0(t1)
+	# coloca o endereço logo apos o nome do rotulo
 	sw t2, 20(t3)
 	
 	la t1, ponteiro_tabela
@@ -243,8 +260,7 @@ salva_endereco_rotulo:
 	addi t2, t2, 24 # avanca pra prox entrada
 	sw t2, 0(t1)
 	
-	addi s1, s1, 1 # pula ':'
-	
+	addi s1, s1, 1 # pula ":"
 	j inicia_nova_palavra
 	
 avalia_espaco:
@@ -270,6 +286,25 @@ trata_secao:
 	li t0, 84 # 'T'
 	beq t5, t0, muda_para_text
 	
+	li t0, 119 # 'w' .word
+	beq t5, t0, muda_para_word
+	li t0, 87 # 'W' WORD
+	beq t5, t0, muda_para_word
+	
+	li t0, 104 # 'h' .half
+	beq t5, t0, muda_para_half
+	li t0, 72 # 'H' .HALF
+	beq t5, t0, muda_para_half
+	
+	li t0, 98 # 'b' .byte
+	beq t5, t0, muda_para_byte
+	li t0, 66 # 'B' .BYTE
+	beq t5, t0, muda_para_byte
+	
+	li t0, 115 # 's' .string
+	beq t5, t0, muda_para_string
+	li t0, 83 # 'S' .STRING
+	beq t5, t0, muda_para_string
 	
 muda_para_data:
 	la t1, secao_atual
@@ -281,6 +316,23 @@ muda_para_text:
 	la t1, secao_atual
 	sw t0, 0(t1)
 	j procura_quebra_linha
+	
+# secao em data
+muda_para_word:
+	addi s1, s1, 5 # incrementa o ponteiro pra pular ".word"
+	j implementacao_word
+	
+muda_para_half:
+	addi s1, s1, 5 # incrementa o ponteiro pra pular ".half"
+	j implementacao_half
+
+muda_para_byte:
+	addi s1, s1, 5 # incrementa o ponteiro pra pular ".byte"
+	j implementacao_byte
+
+muda_para_string:
+	addi s1, s1, 7 # incrementa o ponteiro pra pular ".byte"
+	j implementacao_string
 
 identifica_instrucao:
 	lbu t5, 0(a0)
@@ -912,6 +964,23 @@ implementacao_AUIPC:
 	
 	la a0, str_auipc
     	j teste
+    	
+# implementacoes data
+implementacao_word:
+	# TODO ler os numeros e quebrar em 4 bytes, chamar processa_byte_data para cada byte
+	j procura_quebra_linha
+	
+implementacao_half:
+	# TODO ler os numeros e quebrar em 2 bytes, chamar processa_byte_data para cada byte
+	j procura_quebra_linha
+
+implementacao_byte:
+	# TODO ler os numero, chamar processa_byte_data para o byte
+	j procura_quebra_linha
+
+implementacao_string:
+	# TODO ler as letras e ate o nulo (0x00), chamar processa_byte_data para cada char
+	j procura_quebra_linha
 
 # -- temporario
 teste:
@@ -984,12 +1053,35 @@ comeca_segunda:
 	la t0, pc_text # zera o pc da text
 	sw zero, 0(t0)
 	
+	la t0, pc_data # zera o pc da data
+	sw zero, 0(t0)
+	
 	la t0, secao_atual # retorna para o comeco da . data
 	sw zero, 0(t0)
 	
 	j inicia_scanner
 
 exit:
+	# verifica passagem atual pra gerar o padding
+	la t0, passagem_atual
+	lw t1, 0(t0)
+	li t2, 2
+	bne t1, t2, finaliza_arquivos 
+	
+	la t0, secao_atual
+	sw zero, 0(t0) # vai pra data para dar o padding
+	
+loop_padding_data:
+	la t0, pc_data
+	lw t1, 0(t0)
+	li t2, 4096 # 1024 linhas x 4 bytes
+	bge t1, t2, finaliza_arquivos
+	
+	li a0, 0 # preenche com 0x00
+	jal ra, processa_byte_data
+	j loop_padding_data
+	
+finaliza_arquivos:
 	la a0, end_arquivo # grava 'END;' nos dois arquivos
 	la a1, buffer_saida_data
 	la a2, ponteiro_saida_data
@@ -1068,6 +1160,18 @@ fim_escreve_string_buffer:
 	
 # procedimento verificar passagem (ret a0 = 0 se segunda passagem. ret a0 = 1 se primeira passagem)
 verifica_primeira_passagem:
+	la t0, secao_atual
+	lw t1, 0(t0)
+	bne t1, zero, incrementa_text
+	j checa_passagem
+	
+incrementa_text:
+	la t0, pc_text
+	lw t1, 0(t0)
+	addi t1, t1, 4
+	sw t1, 0(t0)
+	
+checa_passagem:
 	la t0, passagem_atual
 	lw t1, 0(t0)
 	
@@ -1077,23 +1181,169 @@ verifica_primeira_passagem:
 	li a0, 0 # segunda passagem
 	jr ra
 	
-primeira_passagem:
-	# verifica se esta na .text
-	la t0, secao_atual
-	lw t1, 0(t0)
-
-	beq t1, zero, primeira_passagem_data
-
-	# incrementa +4 pois cada instrução anda 4 enderecos
-	la t0, pc_text
-	lw t1, 0(t0)
-	addi t1, t1, 4
-	sw t1, 0(t0)
-	
+primeira_passagem:	
 	li a0, 1
 	jr ra
 	
-primeira_passagem_data:
-	li a0, 1
+# procedimento
+# converte inteiro (em a0) para string hex
+# a1 = end de destino
+
+int_to_hex_string:
+	li t0, 8 # contador de digitos
+	add t1, a0, zero # armazena int em a1
+	add t2, a1, zero # ponteiro de gravacao
+
+loop_hex:
+	srli t3, t1, 28 # coloca em t3 o nibble mais significativo
+	andi t3, t3, 0xF # salva apenas o nible a direita
+	
+	li t4, 9
+	ble t3, t4, num_hex
+	
+letra_hex:
+	addi t3, t3, 55 # casa com o correspondente a A - F na table ascii
+	j salva_hex
+	
+num_hex:
+	addi t3, t3, 48 # casa com correspondende 0 - 9 na table ascii
+	
+salva_hex:
+	sb t3, 0(t2) # salva char no buffer
+	 slli t1, t1, 4 # bits processados sao descartados
+	 addi t2, t2, 1 # avanca ponteiro buffer
+	 addi t0, t0, -1 # avanca contador (diminui)
+	 bne t0, zero, loop_hex
+	 
+	 sb zero, 0(t2) # null no fim da string
+	 jr ra
+	 
+# procedimento
+# guardar linha no .mif ("end : instrucao;\n")
+# a0 = instrucao binaria
+grava_linha_mif:
+	#reserva 2 espacos na pilha
+	addi sp, sp, -8
+	sw ra, 4(sp) # salva ra atual na pilha
+	sw a0, 0(sp) # salva instrucao na pilha
+	
+	la t0, secao_atual # escolhe o pc da secao atual
+	lw t1, 0(t0)
+	beq t1, zero, pc_data_mif
+	
+pc_text_mif:
+	la t0, pc_text
+	lw t2, 0(t0)
+	la a1, buffer_saida_text
+	la a2, ponteiro_saida_text
+	j calcula_end_mif
+	
+pc_data_mif:
+	la t0, pc_data
+	lw t2, 0(t0)
+	la a1, buffer_saida_data
+	la a2, ponteiro_saida_data
+	j calcula_end_mif
+
+calcula_end_mif:
+	# voltamos 4 no pc pois a verifica_passagem já somou 4
+	addi t2, t2, -4
+	# dividimos por 4 usando o deslocamento para esquerda duas vezes
+	srli a0, t2, 2 # fazemos isso porque o arquivo .mif anda de 1 em 1 ao inves de 4 em 4
+	
+	# salvamos o buffer e ponteiro na pila
+	addi sp, sp, -8
+	sw a1, 4(sp)
+	sw a2, 0 (sp)
+	
+	la a1, buffer_hex
+	jal ra, int_to_hex_string # converte o endereco para hex
+	# grava
+	la a0, buffer_hex
+	lw a1, 4(sp)
+	lw a2, 0(sp)
+	jal ra, escreve_string_buffer # grava no buffer de saida
+	
+	la a0, str_dois_pnts
+	lw a1, 4(sp)
+	lw a2, 0(sp)
+	jal ra, escreve_string_buffer # grava no buffer de saida
+	
+	# recuperamos a a0 inicial (instrucao)
+	lw a0, 8(sp)
+	la a1, buffer_hex
+	jal ra, int_to_hex_string # converte a instrucao
+	la a0, buffer_hex
+	lw a1, 4(sp)
+	lw a2, 0(sp)
+	jal ra, escreve_string_buffer  # grava instrucao no buffer de saida
+
+	# grava, por fim ";\n"
+	la a0, str_pnt_virg
+	lw a1, 4(sp)
+	lw a2, 0(sp)
+	jal ra, escreve_string_buffer
+	
+	# recupera a pilha
+	addi sp, sp, 8
+	lw a0, 0(sp)
+	lw ra, 4(sp)
+	addi sp, sp, 8
+	jr ra
+	
+# procedimento
+# processa o byte para adicionar em .data
+# a0 = byte em hex a ser processado
+
+processa_byte_data:
+	addi sp, sp, -16 # abre 4 espacos na pilha
+	sw ra, 12(sp) # salva ra
+	sw a0, 8(sp) # salva byte a ser processado
+	
+	la t0, pc_data # incrementa pc_data
+	lw t1, 0(t0)
+	addi t1, t1, 1
+	sw t1, 0(t0)
+	
+	# se for primeira passagem, nao continua
+	jal ra, verifica_primeira_passagem
+	li t0, 1
+	beq a0, t0, pb_fim
+	
+pb_segunda_passagem:
+	lw a0, 8(sp) # recupera o byte passado
+	
+	la t0, contador_bytes_dados
+	lw t1, 0(t0) # olha quantos bytes já estão processados
+	
+	slli t2, t1, 3 # multiplica o contador por 8 para achar quanto precisa deslocar os 32 bits
+	sll a0, a0, t2
+	
+	la t3, acumulador_dados
+	lw t4, 0(t3)
+	or t4, t4, a0 # com a ou ele realiza uma "colagem" no acumulador
+	sw t4, 0(t3)
+	
+	addi t1, t1, 1 # prox byte
+	sw t1, 0(t0)
+	
+	# verifica se fechou a word
+	li t5, 4
+	bne t1, t5, pb_fim
+	
+	# se fechou, grava a linha
+	add a0, t4, zero # a0 recebe a word montada
+	jal ra, grava_linha_mif
+	
+	# esvazia para prox word
+	la t0, contador_bytes_dados
+	sw zero, 0(t0)
+	la t3, acumulador_dados
+	sw zero, 0(t3)
+	
+pb_fim:
+	# recupera pilha
+	lw ra, 12(sp)
+	addi sp, sp, 16
 	jr ra
 	
